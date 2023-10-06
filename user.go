d5257a743816cc171f2d1862e1b020cb38da1a3b
vineela <vineela.devarashetty@gmail.com>
2023-10-06 13:13:21 -0400

    Delete user.go

--- user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	UserID string `json:"UserID"`
-	Name   string `json:"Name"`
-	Email  string `json:"Email"`
-	DOB    string `json:"DOB"`
-	// Add other fields as needed
-}
